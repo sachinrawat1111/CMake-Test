ddc21b607c7b6453f8f48b6a50d36e5a1e828818
sachinrawat1111 <sachin.rawat@stryker.com>
2020-11-25 21:49:39 +0530

    Commit

---
+++ CMakeTest/src/Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, e;
+	cout << "Enter two numbers:\n";
+	cin >> a >> b;
+	c = a + b;
+	cout << "Sum = "<< c;
+	cout << "\nEnter any number to exit: ";
+	cin >> e;
+	return 0;
+}
